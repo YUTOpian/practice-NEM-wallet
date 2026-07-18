10800defa407ef31bc50a385fe85c5f76a962ea8
YUTO <63623900+YUTOpian@users.noreply.github.com>
2026-07-18 23:40:08 +0900

    Create mnemonic.js

---
+++ js/mnemonic.js
@@ -0,0 +1,22 @@
+// mnemonic.js
+// BIP39 ニーモニック管理
+
+import { sha256 } from "./utils.js";
+
+
+/*
+  ニーモニック生成
+*/
+export function createMnemonic() {
+
+  // 後でbip39ライブラリ利用
+
+}
+
+
+/*
+  ニーモニックから秘密鍵生成
+*/
+export function mnemonicToPrivateKey(mnemonic) {
+
+}
